2bb80af28ac6f5f44222f3553b4a1c9ea7e24f7d
jeffchengtw <jeff.cheng.cs10@nycu.edu.tw>
2023-07-04 09:42:12 +0800

    predict

--- predictor.py
+++ predictor.py
@@ -13,9 +13,9 @@ from option.util import*
 
 if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    test_data_dir = r'D:\GAN\mine\dataset\dot_char_crop_dot'
-    history_dir = r'history\dot_char_crop_dot\202306291744_epoch500numD2_inputc1_outputc3'
-    ckpt_dir = os.path.join(history_dir, 'ckpt', 'epoch_100.pt')
+    test_data_dir = r'D:\GAN\mine\dataset\dot_char_crop_simple'
+    history_dir = r'history\dot_char_crop_simple\202307010707_epoch500numD2_inputc1_outputc3'
+    ckpt_dir = os.path.join(history_dir, 'ckpt', 'epoch_480.pt')
     config_dir = os.path.join(history_dir, 'config.txt')
     settings = Settings(config_dir)
 
@@ -25,15 +25,28 @@ if __name__ == '__main__':
     num_D = settings.num_d
     n_layer_D = settings.n_layer_d
     input_nc = settings.input_nc
+    input_h = settings.input_h
+    input_w = settings.input_w
+    label_nc = settings.label_nc
     output_nc = settings.output_nc
     n_downsampling = settings.n_downsampling
 
     # test dataloader
-    test_dataset = MyDataset(test_data_dir, 'test')
+    test_dataset = MyDataset(test_data_dir, 'test', (input_w, input_h))
     test_loader = DataLoader(test_dataset, batch_size=1)
 
-    netG = Generator(input_nc=input_nc, output_nc=output_nc, n_downsampling=n_downsampling, cfg=settings).to(device)
-    netD = MultiscaleDiscriminator(input_nc=(0+output_nc), num_D=num_D, n_layers=n_layer_D).to(device)
+    netG = Generator(
+        input_nc=label_nc, 
+        output_nc=output_nc, 
+        n_downsampling=n_downsampling, 
+        cfg=settings
+    ).to(device)
+    
+    netD = MultiscaleDiscriminator(
+        input_nc=(output_nc+label_nc),
+        num_D=num_D, 
+        n_layers=n_layer_D
+    ).to(device)
 
     ckpt = torch.load(ckpt_dir)
     netG.load_state_dict(ckpt['generator'])
